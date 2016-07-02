a575cea76d6e828727862300ec0067118058cc70
Patryk Wertka <spartanpageagain@gmail.com>
2016-07-02 21:19:45 +0200

    added attributes adding for tree & nodes

--- source/tsdslhelpers.pas
+++ source/tsdslhelpers.pas
@@ -33,16 +33,19 @@ procedure AddNodesToTree(
   const Nodes: NamedNodes
 );
 var NNode: NamedNode;
+var GluedPath: String;
 begin
   for NNode in Nodes do
   begin
+    GluedPath := GluePath(Path, NNode.Name.Value);
     Tree.CreateChildNode(Path, False, NNode.Name.Value);
     Tree.WriteChildNodeComment(
-      GluePath(Path, NNode.Name.Value),
+      GluedPath,
       NNode.Comment.Value,
       NNode.Comment.Delimeter,
       ctDeclaration
     );
+    AddAttributesToTree(Tree, GluedPath, NNode.Attributes);
   end;
 end;
 
@@ -51,8 +54,21 @@ procedure AddAttributesToTree(
   const Path:  String;
   const Attrs: NamedAttributes
 );
+var Attr: NamedAttribute;
+var GluedPath: String;
 begin
-  //todo;
+  for Attr in Attrs do
+  begin
+    GluedPath := GluePath(Path, Attr.Name.Value);
+    Tree.CreateAttribute(Path, False, Attr.Name.Value);
+    Tree.WriteAttributeComment(
+      GluedPath,
+      Attr.Comment.Value,
+      Attr.Comment.Delimeter,
+      ctDeclaration
+    );
+    Tree.WriteString(GluedPath, Attr.Content.Value);
+  end;
 end;
 
 function GluePath(
@@ -63,6 +79,8 @@ function GluePath(
 begin
   if Rhs = '' then
     GluePath := Lhs
+  else if Lhs = '' then
+    GluePath := Rhs
   else
     GluePath := Lhs + Delimeter + Rhs;
 end;

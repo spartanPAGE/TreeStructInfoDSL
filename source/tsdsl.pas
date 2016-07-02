0f01755f914c747fa89174718d302d021e1a9dd2
Patryk Wertka <spartanpageagain@gmail.com>
2016-07-02 13:51:11 +0200

    Added TreeStructInfo (Name)/(Name, Comment) function with Name/Comment functions related to StrictName/StrictComment

--- source/tsdsl.pas
+++ source/tsdsl.pas
@@ -5,16 +5,53 @@ unit tsdsl;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, TSInfoFiles, TSInfoConsts, TSInfoTypes;
 
-function test: Integer;
+type StrictName = record
+  Value: String;
+end;
+
+type StrictComment = record
+  Value: String;
+  Delimeter: String;
+end;
+
+function Name(const Value: String): StrictName;
+
+function Comment(const Value: String; const Delimeter: String = ''): StrictComment;
+
+function TreeStructInfo(const Name: StrictName): TTSInfoTree; overload;
+function TreeStructInfo(
+  const Name: StrictName;
+  const Comment: StrictComment
+): TTSInfoTree; overload;
 
 implementation
 
-
-function test: Integer;
+function Name(const Value: String): StrictName;
 begin
-  test := 42;
+  Name.Value := Value;
+end;
+
+function Comment(const Value: String; const Delimeter: String): StrictComment;
+begin
+  Comment.Value := Value;
+  Comment.Delimeter := Delimeter;
+end;
+
+function TreeStructInfo(const Name: StrictName): TTSInfoTree;
+begin
+  TreeStructInfo := TTSInfoTree.Create;
+  TreeStructInfo.RenameTree(Name.Value);
+end;
+
+function TreeStructInfo(
+  const Name: StrictName;
+  const Comment: StrictComment
+): TTSInfoTree;
+begin
+  TreeStructInfo := TreeStructInfo(Name);
+  TreeStructInfo.WriteTreeComment(Comment.Value, '');
 end;
 
 end.

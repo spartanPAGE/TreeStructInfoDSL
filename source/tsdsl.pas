2974c4d5ac2f606e7863d72be2213da8db34a486
Patryk Wertka <spartanpageagain@gmail.com>
2016-07-02 12:39:25 +0200

    added TreeStructInfoDSL package with sample source file

---
+++ source/tsdsl.pas
@@ -0,0 +1,21 @@
+unit tsdsl;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+function test: Integer;
+
+implementation
+
+
+function test: Integer;
+begin
+  test := 42;
+end;
+
+end.
+
